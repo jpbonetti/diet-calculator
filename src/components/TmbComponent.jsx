f3d852c2137d988c62c26f5b77e1fe6b25bae12f
Joao Paulo Bonetti <jpbr.webdesigner@gmail.com>
2020-07-31 19:30:53 -0300

    Finalized tmb calculate logic

--- src/components/TmbComponent.jsx
+++ src/components/TmbComponent.jsx
@@ -1,6 +1,10 @@
 import React, { Component } from 'react';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
+import Radio from '@material-ui/core/Radio';
+import RadioGroup from '@material-ui/core/RadioGroup';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import FormLabel from '@material-ui/core/FormLabel';
 import './TmbComponent.css';
 
 export default class TmbComponent extends Component {
@@ -9,6 +13,7 @@ export default class TmbComponent extends Component {
     age: undefined,
     weight: undefined,
     height: undefined,
+    gender: undefined,
     result: 0
   }
 
@@ -22,10 +27,19 @@ export default class TmbComponent extends Component {
     this.tbmbnformations[e.target.name] = parseFloat(e.target.value);
   }
 
-  calculate() {
+  handleChangeRadioButtons(e) {
+    this.tbmbnformations.gender = e.target.value;
+  }
 
-    console.log("HOMENS: " + 66 + (13.8 * this.tbmbnformations.weight) + (5 * this.tbmbnformations.height) - (6.8 * this.tbmbnformations.age));
-    console.log("MULHERES: " + 655 + (9.6 * this.tbmbnformations.weight) + (1.8 * this.tbmbnformations.height) - (4.7 * this.tbmbnformations.age));
+  calculate() {
+    debugger;
+    if (this.tbmbnformations.gender === undefined || this.tbmbnformations.gender === 'male') {
+      this.tbmbnformations.result = 66 + (13.8 * this.tbmbnformations.weight) + (5 * this.tbmbnformations.height) - (6.8 * this.tbmbnformations.age);
+    } else {
+      this.tbmbnformations.result = 655 + (9.6 * this.tbmbnformations.weight) + (1.8 * this.tbmbnformations.height) - (4.7 * this.tbmbnformations.age);
+    }
+
+    window.alert(this.tbmbnformations.result);
   }
 
   render() {
@@ -67,6 +81,13 @@ export default class TmbComponent extends Component {
             value={this.tbmbnformations.height}
             onChange={this.handleChangeTextFields.bind(this)}
           />
+        </div>
+        <div>
+            <FormLabel component="legend">Sexo</FormLabel>
+            <RadioGroup defaultValue="male" aria-label="gender" name="gender1" value={this.tbmbnformations.gender} onChange={this.handleChangeRadioButtons.bind(this)}>
+              <FormControlLabel value="male" control={<Radio />} label="Male" />
+              <FormControlLabel value="female" control={<Radio />} label="Female" />
+            </RadioGroup>
         </div>
         <div>
           <Button 
