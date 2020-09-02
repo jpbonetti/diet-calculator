f6e69a1a0a8a469a23dc441f4d84e6700d4d6888
Joao Paulo Bonetti <jpbr.webdesigner@gmail.com>
2020-09-02 20:04:08 -0300

    Finished result component

--- src/components/ResultCalculate.jsx
+++ src/components/ResultCalculate.jsx
@@ -34,14 +34,14 @@ export default class ResultCalculateComponent extends Component {
     if (this.state.tmbInformations.tmbResult) {
       var totalCalories = this.state.tmbInformations.tmbResult + this.state.tmbInformations.exerciseResult;
 
-      this.state.resultCalculate.decrease.min = (totalCalories - 500).toFixed(2);
-      this.state.resultCalculate.decrease.max = (totalCalories - 200).toFixed(2);
+      this.state.resultCalculate.decrease.min = (totalCalories - 500).toFixed(2).replace(".", ",");
+      this.state.resultCalculate.decrease.max = (totalCalories - 200).toFixed(2).replace(".", ",");
 
-      this.state.resultCalculate.keep.min = (totalCalories - 100).toFixed(2);
-      this.state.resultCalculate.keep.max = (totalCalories).toFixed(2);
+      this.state.resultCalculate.keep.min = (totalCalories - 100).toFixed(2).replace(".", ",");
+      this.state.resultCalculate.keep.max = (totalCalories).toFixed(2).replace(".", ",");
 
-      this.state.resultCalculate.increase.min = (totalCalories + 100).toFixed(2);
-      this.state.resultCalculate.increase.max = (totalCalories + 500).toFixed(2);
+      this.state.resultCalculate.increase.min = (totalCalories + 100).toFixed(2).replace(".", ",");
+      this.state.resultCalculate.increase.max = (totalCalories + 500).toFixed(2).replace(".", ",");
     }
   }
 
@@ -62,33 +62,33 @@ export default class ResultCalculateComponent extends Component {
               className="card-information">
               <div className="title-card-information text-shadow">Perder peso</div>
               <div className="result-card-information text-shadow">
-                {this.state.resultCalculate.decrease.min} até {this.state.resultCalculate.decrease.max} calorias
+                {this.state.resultCalculate.decrease.min} até {this.state.resultCalculate.decrease.max} calorias por dia
               </div>
               
               <img className="image-card-information" src={decreaseIcon}/>
-              <div className="descryption-card-information">Perca de 0,5kg a 1kg por semana</div>
+              <div className="descryption-card-information">Essa janela de calorias permite que você perca de 0,5kg por semana</div>
             </Paper>
             <Paper 
               elevation={5}
               className="card-information">
               <div className="title-card-information text-shadow">Manter peso</div>
               <div className="result-card-information text-shadow">
-                {this.state.resultCalculate.keep.min} até {this.state.resultCalculate.keep.max} calorias
+                {this.state.resultCalculate.keep.min} até {this.state.resultCalculate.keep.max} calorias por dia
               </div>
 
               <img className="image-card-information" src={keepIcon}/>
-              <div className="descryption-card-information">Mantenha o peso</div>
+              <div className="descryption-card-information">Essa janela de calorias permite que você mantenha o seu peso</div>
             </Paper>
             <Paper 
               elevation={5}
               className="card-information">
               <div className="title-card-information text-shadow">Ganhar peso</div>
               <div className="result-card-information text-shadow">
-                {this.state.resultCalculate.increase.min} até {this.state.resultCalculate.increase.max} calorias
+                {this.state.resultCalculate.increase.min} até {this.state.resultCalculate.increase.max} calorias por dia
               </div>
 
               <img className="image-card-information" src={increaseIcon}/>
-              <div className="descryption-card-information">Ganhe de 0,5kg a 1kg por semana</div>
+              <div className="descryption-card-information">Essa janela de calorias permite que você ganhe até 0,5kg por semana</div>
             </Paper>
           </div>
         </div>
